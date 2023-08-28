68871c854d61a12ba5b2d7111c0df98f3a94c85e
ErickMoseDev <erickmongaremose@gmail.com>
2023-08-28 12:32:35 +0300

    feat: complete lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+  validate :clickbait_title?
+
+  def clickbait_title?
+    clickbaits =
+      [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i,
+      ]
+    if clickbaits.none? { |clickbait| clickbait.match title }
+      errors.add(:title, "Title is not clickbait enough")
+    end
+  end
 end
